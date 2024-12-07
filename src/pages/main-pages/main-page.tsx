9316da912d799a414ef9b7aa2547ba6d2213d678
silentji <itihomirov156@gmail.com>
2024-12-08 00:48:29 +0500

    added sorting form in main

--- src/pages/main-pages/main-page.tsx
+++ src/pages/main-pages/main-page.tsx
@@ -7,19 +7,41 @@ import { Helmet } from 'react-helmet-async';
 import { useAppSelector } from '@hooks/index';
 import { CitiesList } from '@components/citiesList';
 import { Offers } from '@appTypes/offer';
+import { SortingForm } from './sorting-form';
 
 export function MainPage(): JSX.Element {
   const offers = useAppSelector((state) => state.offersList);
   const city = useAppSelector((state) => state.city);
 
   const [currentCityOffers, setCurrentCityOffers] = useState<Offers>([]);
+  const [filteredCityOffers, setFilteredCityOffers] = useState<Offers>([]);
   const [activeOfferId, setActiveOfferId] = useState<string | null>(null);
 
   const activeOffer = offers.find((offer) => offer.id === activeOfferId);
   useEffect(() => {
     const filteredOffers = offers.filter((offer) => offer.city.name === city.name);
+    setFilteredCityOffers(filteredOffers);
     setCurrentCityOffers(filteredOffers);
   }, [city, offers]);
+
+  const handleSortChange = (selectedSort: string) => {
+    let sortedOffers = [...currentCityOffers];
+    switch (selectedSort) {
+      case 'Price: low to high':
+        sortedOffers.sort((a, b) => a.price - b.price);
+        break;
+      case 'Price: high to low':
+        sortedOffers.sort((a, b) => b.price - a.price);
+        break;
+      case 'Top rated first':
+        sortedOffers.sort((a, b) => b.rating - a.rating);
+        break;
+      default:
+        sortedOffers = [...filteredCityOffers];
+        break;
+    }
+    setCurrentCityOffers(sortedOffers);
+  };
 
   return (
     <div className="page page--gray page--main">
@@ -39,21 +61,7 @@ export function MainPage(): JSX.Element {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{`${currentCityOffers.length} places to stay in ${city.name}`}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <SortingForm onSortChange={handleSortChange}/>
               <OffersList offers={currentCityOffers} onActiveOfferChange={setActiveOfferId} className='cities__places-list places__list tabs__content'/>
             </section>
             <div className="cities__right-section">
---
+++ src/pages/main-pages/sorting-form.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+type SortingFormProps = {
+  onSortChange: (selectedSort: string) => void;
+};
+
+export function SortingForm({ onSortChange }: SortingFormProps): JSX.Element {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedSort, setSelectedSort] = useState('Popular');
+
+  const sortingOptions = [
+    'Popular',
+    'Price: low to high',
+    'Price: high to low',
+    'Top rated first'
+  ];
+
+  const handleSortSelection = (option: string) => {
+    setSelectedSort(option);
+    setIsOpen(false);
+    onSortChange(option);
+  };
+
+  return (
+    <form className="places__sorting">
+      <span className="places__sorting-caption">Sort by</span>
+      <span 
+        className="places__sorting-type" 
+        tabIndex={0}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selectedSort}
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      {isOpen && (
+        <ul className="places__options places__options--custom places__options--opened">
+          {sortingOptions.map((option) => (
+            <li
+              key={option}
+              className={`places__option ${option === selectedSort ? 'places__option--active' : ''}`}
+              tabIndex={0}
+              onClick={() => handleSortSelection(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </form>
+  );
+}
