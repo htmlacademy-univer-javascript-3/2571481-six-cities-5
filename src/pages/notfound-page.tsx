4bbd9ede79394edc0886e25ba9e2764b6ac6bca0
silentji <itihomirov156@gmail.com>
2024-12-11 00:24:29 +0500

    not found page cosmetics

--- src/pages/notfound-page.tsx
+++ src/pages/notfound-page.tsx
@@ -1,15 +1,31 @@
+import Footer from '@components/footer';
+import Header from '@components/header';
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 export function NotFoundPage(): JSX.Element {
   return(
     <Fragment>
-      <h1>
-        404.
-        <br />
-        <small>Page not found</small>
-      </h1>
-      <Link to="/"><u>Go to main page</u></Link>
+      <Header/>
+      <div style={styles.container}>
+        <h1>
+          404.
+          <br />
+          <small>Page not found</small>
+        </h1>
+        <Link to="/"><u>Go to main page</u></Link>
+      </div>
+      <Footer/>
     </Fragment>
   );
 }
+
+const styles: { container: React.CSSProperties } = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontSize: '45px',
+    height: '700px',
+  },
+};
